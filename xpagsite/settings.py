50dfe92309f9e512b5b5d33381028df10a18ebb9
Diedrick <KEDIEDRICK@capousd.org>
2017-07-07 08:18:50 -0700

    email backend attempt

--- xpagsite/settings.py
+++ xpagsite/settings.py
@@ -120,7 +120,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'sendgrid_username'
+EMAIL_HOST_PASSWORD = 'sendgrid_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 MESSAGE_TAGS = {
